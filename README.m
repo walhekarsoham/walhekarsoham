3d05cbe03e37183e7ae0613523867513bc007949
walhekarsoham <147260835+walhekarsoham@users.noreply.github.com>
2023-10-07 20:19:21 +0530

    Update README.md

--- README.m
+++ README.m
@@ -1,5 +1,5 @@
 HI,I'm @walhekarsoham.
 I'm CSE student.
-I'm currently learning python.
+I'm currently learning on python.
 I'm looking to collaborate on python based projects.
 gmail=walhekarsoham07@gmail.com
