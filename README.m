1742e9e0b7f8aef6dbc46e27969c357bc9fafaa7
walhekarsoham <147260835+walhekarsoham@users.noreply.github.com>
2023-10-07 20:18:33 +0530

    Update README.md

--- README.m
+++ README.m
@@ -2,13 +2,4 @@ HI,I'm @walhekarsoham.
 I'm CSE student.
 I'm currently learning python.
 I'm looking to collaborate on python based projects.
-gmail=walhekarsoham07@gmail.com- 👋 Hi, I’m @walhekarsoham
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
-
-<!---
-walhekarsoham/walhekarsoham is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+gmail=walhekarsoham07@gmail.com
